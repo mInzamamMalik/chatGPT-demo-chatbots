4e10306345bc3bdb0369f9c80b275972031fb844
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-09-01 01:12:15 +0500

    fixed setShowProfileForm

--- frontend/app/chat/page.jsx
+++ frontend/app/chat/page.jsx
@@ -1,10 +1,22 @@
+"use client";
+import { useEffect, useState } from "react";
 import { ChatFrom, SetUserProfile } from "@/components";
 
 export default function page() {
+  const [showProfileForm, setShowProfileForm] = useState(null);
+
+  useEffect(() => {
+    const profile = localStorage.getItem("profile");
+    profile ? setShowProfileForm(false) : setShowProfileForm(true);
+  }, []);
+
   return (
     <main className="bg-cover h-full bg-fixed bg-center bg-no-repeat">
-      <SetUserProfile />
-      <ChatFrom />
+      {showProfileForm === null && "Loading..."}
+      {showProfileForm && (
+        <SetUserProfile setShowProfileForm={setShowProfileForm} />
+      )}
+      {showProfileForm === false && <ChatFrom />}
     </main>
   );
 }
